c4a956b193d0ab5d1fe6cbd2ee1c44275b0915a9
Yavuz <kutuk.y@expoweb.fr>
2019-05-23 12:40:46 +0200

    Dojo sur la récursivité

---
+++ README.md
@@ -0,0 +1,10 @@
+# DOJO Récursivité
+
+##### Enoncé
+
+Écrire une fonction qui prend un entier en entrée et qui renvoie son écriture en nombre romain
+
+##### Exemple : Pour transposer :
+* 85 : 80 +5 = LXXX + V = LXXXV
+* 697 : 600 + 90 + 7 = DC + XC + VII = DCXCVII
+* 4877 : 4000 + 800 + 70 + 7 = MMMM + DCCC + LXX + VII = MMMMDCCCLXXVII
---
+++ Recursive.php
@@ -0,0 +1,30 @@
+<?php
+function toRoman($nombreArabe, $nombreRomain = ""){
+    if ($nombreArabe<=0) return $nombreRomain;
+
+    $tab = [1=>"I",
+        4=>"IV",
+        5=>"V",
+        9=>"IX",
+        10=>"X",
+        50=>"L",
+        100=>"C",
+        500=>"D",
+        900=>"CM",
+        1000=>"M"
+    ];
+
+    $keys = array_keys($tab);
+    $i = count($keys) - 1;
+
+
+    while($keys[$i] > $nombreArabe){
+        $i--;
+    }
+
+    //echo $keys[$i].'-'.$nombreArabe.'-'.$nombreRomain.'<hr>';
+
+    return toRoman($nombreArabe - $keys[$i], $nombreRomain . $tab[$keys[$i]]);
+}
+
+echo toRoman(1234);
